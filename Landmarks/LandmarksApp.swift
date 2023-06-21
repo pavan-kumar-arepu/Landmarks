41b1c346b438d725347286d4ad1c8692b2fa555d
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2023-06-21 11:39:52 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Pavankumar Arepu on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
